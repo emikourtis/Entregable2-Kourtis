ea1b8a3f5c8fc23fcc668306727d67d9307ca951
emikourtis <126197808+emikourtis@users.noreply.github.com>
2023-05-12 09:02:46 -0300

    problema con compra

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  
+  pedirProductos();
+});
 //carrito empieza con un array vacio al que le agregaremos productos o le sacaremos
 let carrito = [];
 
 //capturo los id del html y guardo en constantes
-const contenedor = document.querySelector("#contenedor");
 const carritoContenedor = document.querySelector("#carritoContenedor");
 const vaciarCarrito = document.querySelector("#vaciarCarrito");
 const precioTotal = document.querySelector("#precioTotal");
@@ -10,17 +14,12 @@ const activarFuncion = document.querySelector("#activarFuncion");
 const procesarCompra = document.querySelector("#procesarCompra");
 const totalProceso = document.querySelector("#totalProceso");
 const formulario = document.querySelector('#procesar-pago')
+const contenedor = document.querySelector("#contenedor");
 
-if (activarFuncion) {
-  activarFuncion.addEventListener("click", procesarPedido);
-};
 
-document.addEventListener("DOMContentLoaded", () => {
-  carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-  mostrarCarrito();
-  document.querySelector("#activarFuncion").click(procesarPedido);
-});
+carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
 //si se llena el formulario correctamente y clickea el boton se envia la compra
 if (formulario) {
   formulario.addEventListener('submit', enviarCompra)
@@ -33,7 +32,7 @@ if (vaciarCarrito) {
     mostrarCarrito();
   });
 };
-//si el carrito esta vacio se muestra msj
+//si el carrito esta vacio se muestra un mensaje
 if (procesarCompra) {
   procesarCompra.addEventListener("click", () => {
     if (carrito.length === 0) {
@@ -49,46 +48,57 @@ if (procesarCompra) {
   });
 };
 
+let stockProductos
 //FETCH y async await para mostrar los productos en pantalla
 
-const URL = "productos.json";
-
+const URL = "./productos.json";
 const pedirProductos = async () => {
-  const resp = await fetch(URL);
-  const data = await resp.json();
-  console.log(data);
-  data.forEach((prod) => {
-    contenedor.innerHTML += `
-    <div class="card mt-3" style="width: 18rem;">
-    <img class="card-img-top mt-2" src="${prod.img}" alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">${prod.nombre}</h5>
-      <p class="card-text">Precio: ${prod.precio}</p>
-      <p class="card-text">Descripcion: ${prod.desc}</p>
-      <p class="card-text">Cantidad: ${prod.cantidad}</p>
-      <button class="btn btn-primary" onclick="agregarProducto(${prod.id})">Comprar Producto</button>
-    </div>
-    </div>
-    `;
-    
-  })
-  
-}
-pedirProductos()
+  try {
+    const resp = await fetch(URL);
+    const data = await resp.json();
+    stockProductos = data
+
+    data.forEach((prod) => {
+      contenedor.innerHTML += `
+        <div class="card mt-3" style="width: 18rem;">
+          <img class="card-img-top mt-2" src="${prod.img}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${prod.nombre}</h5>
+            <p class="card-text">Precio: ${prod.precio}</p>
+            <p class="card-text">Descripcion: ${prod.desc}</p>
+            <p class="card-text">Cantidad: ${prod.cantidad}</p>
+            <button class="btn btn-primary" onclick="agregarProducto(${prod.id})">Comprar Producto</button>
+          </div>
+        </div>
+      `;
+    });
+  } catch (err) {
+    console.error('Ocurrió un error: ' + err);
+  } finally {
+    console.log('llamada a la API realizada con exito')
+  }
+};
+
+
+
+
+
 
 //agrego productos al carrito
 const agregarProducto = (id) => {
-  const existe = carrito.some(prod => prod.id === id)
+  const existe = carrito.some(prod => prod.id == id)
 
   if (existe) {
-    const prod = carrito.map(prod => {
-      if (prod.id === id) {
+    carrito.forEach(prod => {
+      if (prod.id == id) {
         prod.cantidad++
       }
     })
   } else {
-    const item = stockProductos.find((prod) => prod.id === id)
-    carrito.push(item)
+    const item = stockProductos.find(prod => prod.id == id)
+    if (item) {
+      carrito.push(item)
+    }
   }
   mostrarCarrito()
 
@@ -100,7 +110,6 @@ const mostrarCarrito = () => {
     modalBody.innerHTML = "";
     carrito.forEach((prod) => {
       const { id, nombre, precio, desc, img, cantidad } = prod;
-      console.log(modalBody);
       modalBody.innerHTML += `
       <div class="modal-contenedor">
         <div id="imagen-carrito">
@@ -118,15 +127,15 @@ const mostrarCarrito = () => {
       `;
     });
   };
-//si no agregaste productos muestro el msj que no se agrego nada
+  //si no agregaste productos muestro el msj que no se agrego nada
   if (carrito.length === 0) {
     modalBody.innerHTML = `
     <p class="text-center text-primary parrafo">¡Aun no agregaste nada!</p>
     `;
-  } 
+  }
   //muestro cantidad de productos
   carritoContenedor.textContent = carrito.length;
-//sumo precios
+  //sumo precios
   if (precioTotal) {
     precioTotal.innerText = carrito.reduce(
       (acc, prod) => acc + prod.cantidad * prod.precio,
@@ -146,12 +155,15 @@ function eliminarProducto(id) {
   carrito = carrito.filter((juego) => juego.id !== juegoId);
   mostrarCarrito();
 };
+
+
+
 //proceder con la compra
 function procesarPedido() {
   carrito.forEach((prod) => {
     const listaCompra = document.querySelector("#lista-compra tbody");
     const { id, nombre, precio, img, cantidad } = prod;
-    if (listaCompra) {
+    
       const row = document.createElement("tr");
       row.innerHTML += `
               <td>
@@ -163,7 +175,7 @@ function procesarPedido() {
             <td>${precio * cantidad}</td>
             `;
       listaCompra.appendChild(row);
-    }
+    
   });
   totalProceso.innerText = carrito.reduce(
     (acc, prod) => acc + prod.cantidad * prod.precio,
@@ -186,10 +198,6 @@ function enviarCompra(e) {
   } else {
 
     const btn = document.getElementById('button');
-
-    // document.getElementById('procesar-pago')
-    //  .addEventListener('submit', function(event) {
-    //    event.preventDefault();
 
     btn.value = 'Enviando...';
 
